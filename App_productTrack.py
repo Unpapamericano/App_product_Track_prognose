f43d4143f60066eb58e4b9736b72941879d8c64c
Doncho <93518242+Unpapamericano@users.noreply.github.com>
2023-10-19 23:22:44 +0200

    Update App_productTrack.py

--- App_productTrack.py
+++ App_productTrack.py
@@ -3,26 +3,21 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.spinner import Spinner
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
+import matplotlib
+matplotlib.use('TkAgg')
 
 kivy.require('2.0.0')
 
 class KpiApp(App):
-
-    def build(self):
-        layout = BoxLayout(orientation='vertical', spacing=10)
-        self.label = Label(text="KPI Dashboard", size_hint_y=None, height=44)
-        btn = Button(text="Zeige Prognose für Gesamtlieferungen", size_hint_y=None, height=44)
-        btn.bind(on_press=self.show_plot)
-        layout.add_widget(self.label)
-        layout.add_widget(btn)
-        return layout
-
-    def on_start(self):
-        # Daten sammeln (normalerweise aus einer Datenbank)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Daten initialisieren
         self.data = {
             'Datum': pd.date_range(start="2023-01-01", periods=30, freq='D'),
             'Gesamtlieferungen': np.random.randint(50, 200, 30),
@@ -30,35 +25,49 @@ class KpiApp(App):
             'DefekteProdukte': np.random.randint(0, 10, 30)
         }
 
+    def build(self):
+        layout = BoxLayout(orientation='vertical', spacing=10)
+        
+        self.label = Label(text="KPI Dashboard", size_hint_y=None, height=44)
+        
+        # Eingabefeld hinzufügen, um Daten zu bearbeiten
+        self.last_data_input = TextInput(hint_text=str(self.data['Gesamtlieferungen'][-1]), size_hint_y=None, height=44)
+        layout.add_widget(Label(text="Letzte Daten von Gesamtlieferungen bearbeiten:", size_hint_y=None, height=44))
+        layout.add_widget(self.last_data_input)
+        
+        # Dropdown-Menü hinzufügen, um die Visualisierung auszuwählen
+        self.spinner = Spinner(text='Liniendiagramm', values=('Liniendiagramm', 'Balkendiagramm', 'Punktdiagramm', 'Histogramm', 'Kreisdiagramm'), size_hint_y=None, height=44)
+        layout.add_widget(self.spinner)
+        
+        btn = Button(text="Visualisierung anzeigen", size_hint_y=None, height=44)
+        btn.bind(on_press=self.show_plot)
+        
+        layout.add_widget(self.label)
+        layout.add_widget(btn)
+        return layout
+
     def show_plot(self, instance):
-        # Erstelle DataFrame aus den gesammelten Daten
-        df = pd.DataFrame(self.data)
-        df['Datum'] = pd.to_datetime(df['Datum'])
-        df.set_index('Datum', inplace=True)
-        df = df.asfreq('D')  # Establecer frecuencia diaria
-
-        # Diferenciación para hacer los datos estacionarios
-        df_diff = df.diff().dropna()
-
-        # ARIMA Prognose für Gesamtlieferungen
-        model = ARIMA(df_diff['Gesamtlieferungen'], order=(5, 1, 0))
-        model_fit = model.fit()
-        forecast = model_fit.forecast(steps=5)
-
-        # Integra el pronóstico para volver a la escala original
-        forecast_original_scale = df['Gesamtlieferungen'].iloc[-1] + forecast.cumsum()
-
-        # Erstelle einen Plot
+        # ... [Code, um Daten zu erhalten und zu verarbeiten] ...
+        
         plt.figure(figsize=(10, 6))
-        df['Gesamtlieferungen'].plot(label="Reale Daten")
-        forecast_original_scale.plot(label="Prognose", linestyle="--")
-        plt.title('Gesamtlieferungen über die Zeit mit Prognose')
-        plt.ylabel('Anzahl')
-        plt.xlabel('Datum')
-        plt.legend()
+        
+        # Auswahl des Visualisierungstyps
+        if self.spinner.text == 'Liniendiagramm':
+            pass  # Dies ist nur ein Platzhalter, hier sollte der Code für das Liniendiagramm eingefügt werden.
+        elif self.spinner.text == 'Balkendiagramm':
+            pass  # Code für Balkendiagramm
+        elif self.spinner.text == 'Punktdiagramm':
+            pass  # Code für Punktdiagramm
+        elif self.spinner.text == 'Histogramm':
+            pass  # Code für Histogramm
+        elif self.spinner.text == 'Kreisdiagramm':
+            pass  # Code für Kreisdiagramm
+        
+        plt.ion()  # Aktiviert den interaktiven Modus
+        plt.draw()  # Zeichnet das Diagramm
+        plt.pause(0.001)  # Ein kurzes Pause, um das GUI-Event zu verarbeiten
         plt.show()
+        plt.ioff()  # Deaktiviert den interaktiven Modus
 
 if __name__ == '__main__':
     KpiApp().run()
-
-
